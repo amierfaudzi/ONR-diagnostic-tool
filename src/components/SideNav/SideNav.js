cdfa44cdfab4d9ea529ce922a6e8da8e07e19e4c
amierfaudzi <amier.faudzi@gmail.com>
2021-02-26 02:04:17 -0500

    add icons to the SideNav component

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './SideNav.css';
+import {ReactComponent as Briefcase} from '../../assets/icons/briefcase.svg';
+import {ReactComponent as Calendar} from '../../assets/icons/calendar.svg';
+import {ReactComponent as Certificate} from '../../assets/icons/certificate.svg';
+import {ReactComponent as Folder} from '../../assets/icons/folder.svg';
+import {ReactComponent as Website} from '../../assets/icons/website.svg';
 
 export default function SideNav() {
     return (
         <div className="side-nav">
-            <div className="icon"></div>
-            <div className="icon"></div>
-            <div className="icon"></div>
-            <div className="icon"></div>
-            <div className="icon"></div>
+            <Briefcase className="icon"/>
+            <Calendar className="icon"/>
+            <Certificate className="icon"/>
+            <Folder className="icon"/>
+            <Website className="icon"/>
         </div>
     )
 }
