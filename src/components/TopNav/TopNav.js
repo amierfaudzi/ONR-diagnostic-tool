0027bedb28ca90f7032d00fd15fb1b20583b2686
amierfaudzi <amier.faudzi@gmail.com>
2021-02-26 12:41:11 -0500

    add icons to the top nav

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './TopNav.css';
+import {ReactComponent as Man} from '../../assets/icons/man.svg';
+import {ReactComponent as Computer} from '../../assets/icons/computer.svg';
+import {ReactComponent as CheckList} from '../../assets/icons/checklist.svg';
+import {ReactComponent as Chart} from '../../assets/icons/chart.svg';
 
 export default function TopNav() {
     return (
@@ -10,11 +14,10 @@ export default function TopNav() {
             <div className="top-nav__sub">
                 <p>Logged in as General User</p>
                 <span className="divider">|</span>
-                <div className="icon-top"></div>
-                <div className="icon-top"></div>
-                <div className="icon-top"></div>
-                <div className="icon-top"></div>
-                <div className="icon-top"></div>
+                <Man className="icon-top"/>
+                <Computer className="icon-top"/>
+                <CheckList className="icon-top"/>
+                <Chart className="icon-top"/>
             </div>
         </div>
     )
